0029695f5778951fdd704a7b6d27b61de3f41bd0
Sebastian Latocha <dev@sebastianlatocha.com>
2019-09-09 23:48:14 +0200

    Master - added a possibility to disable console logging

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import {DEBUG_LOGGING} from './app/helper/Constants';
+
+// disable global console logging if not in debug mode
+if (!DEBUG_LOGGING) { console.log = () => {}; } 
+
 AppRegistry.registerComponent(appName, () => App);
