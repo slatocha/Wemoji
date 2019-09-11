5b413606071103fcb0012dd3bfa09715307c47bc
Sebastian Latocha <dev@sebastianlatocha.com>
2019-09-11 23:38:01 +0200

    MAster - applied modal style

--- app/custom/ErrorModal.js
+++ app/custom/ErrorModal.js
@@ -1,12 +1,14 @@
 import React, { memo, useState, useEffect } from 'react';
-import { Overlay } from 'react-native-elements';
-import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import { Overlay, Button } from 'react-native-elements';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { ERROR_RESET } from '../helper/Error';
 
 import { setError } from '../redux/reducer';
 import { getError } from '../redux/selectors';
+
+import { COLOR_WHITE, COLOR_PASTEL_BLUE } from '../helper/Colors';
 
 // show an custom alert like modal ocerlay if needed
 const ErrorModal = memo(() => {
@@ -23,19 +25,55 @@ const ErrorModal = memo(() => {
       // transparent={false}
       onBackdropPress={() => { if (error.show) dispatch(setError(ERROR_RESET)) }}
       onRequestClose={() => { if (error.show) dispatch(setError(ERROR_RESET)) }}
+      overlayBackgroundColor={'rgba(249, 102, 94, 1)'}
+      width="auto"
+      height={200}
+      borderRadius={12}
       >
-      <View>
-        <Text>{error.title}</Text>
-        <Text>{error.msg}</Text>
-        <TouchableHighlight
+      <View style={styles.body}>
+        <Text style={styles.titleText}>{error.title}</Text>
+        <Text style={styles.msgText}>{error.msg}</Text>
+        <Button
+          style={styles.button}
+          title="OK"
+          
+          // type="outline"
           onPress={() => {
             dispatch(setError(ERROR_RESET))
-          }}>
-          <Text>OK</Text>
-        </TouchableHighlight>
+          }}
+        />
       </View>
     </Overlay>
   );
 });
 
 export default ErrorModal;
+
+const styles = StyleSheet.create({
+  body: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleText: {
+    color:'#fff',
+    fontSize: 20,
+    fontWeight: '800',
+    textAlign: 'center',
+    paddingTop:25,
+    paddingBottom:10,
+    paddingHorizontal: 24,
+  },
+  msgText: {
+    color:'#fff',
+    fontSize: 15,
+    fontWeight: '600',
+    textAlign: 'center',
+    paddingTop:20,
+    paddingHorizontal: 24,
+  },
+  button: {
+    paddingTop:20,
+    width:150
+  }
+});
